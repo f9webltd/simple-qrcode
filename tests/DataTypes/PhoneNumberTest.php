a399ded535aa742d75e4e024a91e02d5962e247d
Robert Allport <rob@f9web.co.uk>
2025-04-19 10:30:09 +0100

    Tighten PhoneNumberTest.php

--- tests/DataTypes/PhoneNumberTest.php
+++ tests/DataTypes/PhoneNumberTest.php
@@ -5,7 +5,7 @@ use F9WebLtd\QrCode\DataTypes\PhoneNumber;
 
 class PhoneNumberTest extends TestCase
 {
-    public function test_it_generates_the_proper_format_for_calling_a_phone_number()
+    public function test_it_generates_the_proper_format_for_calling_a_phone_number(): void
     {
         $phoneNumber = new PhoneNumber();
         $phoneNumber->create(['555-555-5555']);

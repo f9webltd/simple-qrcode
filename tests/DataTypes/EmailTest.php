998652ce86269fea47c43710449d264d70ab7475
Robert Allport <rob@f9web.co.uk>
2025-04-19 10:33:57 +0100

    Tighten EmailTest.php

--- tests/DataTypes/EmailTest.php
+++ tests/DataTypes/EmailTest.php
@@ -18,7 +18,7 @@ class EmailTest extends TestCase
 
         $properFormat = 'mailto:foo@bar.com';
 
-        $this->assertEquals($properFormat, strval($this->email));
+        $this->assertEquals($properFormat, \strval($this->email));
     }
 
     public function test_it_generates_the_proper_format_when_an_email_subject_and_body_are_supplied(): void
@@ -27,7 +27,7 @@ class EmailTest extends TestCase
 
         $properFormat = 'mailto:foo@bar.com?subject=foo&body=bar';
 
-        $this->assertEquals($properFormat, strval($this->email));
+        $this->assertEquals($properFormat, \strval($this->email));
     }
 
     public function test_it_generates_the_proper_format_when_an_email_and_subject_are_supplied(): void
@@ -36,7 +36,7 @@ class EmailTest extends TestCase
 
         $properFormat = 'mailto:foo@bar.com?subject=foo';
 
-        $this->assertEquals($properFormat, strval($this->email));
+        $this->assertEquals($properFormat, \strval($this->email));
     }
 
     public function test_it_generates_the_proper_format_when_only_a_subject_is_provided(): void
@@ -45,7 +45,7 @@ class EmailTest extends TestCase
 
         $properFormat = 'mailto:?subject=foo';
 
-        $this->assertEquals($properFormat, strval($this->email));
+        $this->assertEquals($properFormat, \strval($this->email));
     }
 
     public function test_it_throws_an_exception_when_an_invalid_email_is_given(): void
